29386faa6e35bfee45af7a7cdf60eb48ede68970
Polina Sokolova <polly.sokolova@icloud.com>
2024-10-08 15:59:29 +0400

    Added project files and updated .gitignore

---
+++ CurlyPaws/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurlyPaws
+//
+//  Created by Полина Соколова on 08.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
