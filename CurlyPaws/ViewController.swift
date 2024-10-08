ebc11516c4d8a98179450db375378ebc47e5d12b
Polina Sokolova <polly.sokolova@icloud.com>
2024-10-08 21:50:47 +0400

    Created a stack view, added an image and a label to it, and configured UI elements

--- CurlyPaws/ViewController.swift
+++ CurlyPaws/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let textLabel = UILabel()
+    private let imageView = UIImageView()
+    private let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+     
+        setupLabel()
+        setupImageView()
+        setupStackView()
+        view.addSubview(stackView)
+        
+        setupLayout()
     }
-
-
+    
+    private func setupLabel() {
+        textLabel.text = "jjj"
+        textLabel.font = .systemFont(ofSize: 28, weight: .heavy)
+        textLabel.textAlignment = .center
+        textLabel.textColor = .black
+    }
+    
+    private func setupImageView() {
+        imageView.image = UIImage(named: "toyPoodle")
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        
+    }
+    
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 1
+        
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(textLabel)
+        
+    }
+    
+   private func setupLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+        ])
+    }
 }
-
