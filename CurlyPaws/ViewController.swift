81bb90e518f067da8f2a5a1a71c41df69a1da7cb
Polina Sokolova <polly.sokolova@icloud.com>
2024-10-09 02:18:11 +0400

    Refined logic for updating image and description display

--- CurlyPaws/ViewController.swift
+++ CurlyPaws/ViewController.swift
@@ -13,52 +13,64 @@ class ViewController: UIViewController {
     private let imageView = UIImageView()
     private let stackView = UIStackView()
     
+    private let dogDataManager = DogDataManager(dogs: DogManager().getDogs())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-     
+        
         setupLabel()
         setupImageView()
         setupStackView()
         view.addSubview(stackView)
-        
         setupLayout()
+        
+        updateView()
     }
     
-    private func setupLabel() {
-        textLabel.text = "jjj"
-        textLabel.font = .systemFont(ofSize: 28, weight: .heavy)
+    private func updateView() {
+        let currentDog = dogDataManager.getCurrentDog()
+        
+        textLabel.text = currentDog.description
+        imageView.image = UIImage(named: currentDog.imageName)
+    }
+}
+    
+// MARK: - Setup View
+private extension ViewController {
+    func setupLabel() {
+        textLabel.numberOfLines = 0
+        textLabel.font = .systemFont(ofSize: 14, weight: .regular)
         textLabel.textAlignment = .center
         textLabel.textColor = .black
     }
     
-    private func setupImageView() {
-        imageView.image = UIImage(named: "toyPoodle")
+    func setupImageView() {
         imageView.layer.cornerRadius = 20
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
-        
     }
     
-    private func setupStackView() {
+    func setupStackView() {
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
-        stackView.alignment = .fill
-        stackView.spacing = 1
+        stackView.alignment = .center
+       stackView.spacing = 20
         
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(textLabel)
         
     }
-    
-   private func setupLayout() {
+}
+    // MARK: - Setup Layout
+extension ViewController {
+   func setupLayout() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
-        ])
+       
+       NSLayoutConstraint.activate([
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+           stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+           stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+       ])
     }
 }
